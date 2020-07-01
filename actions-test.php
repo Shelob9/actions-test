7bfbcc5727c18e7bcb5546f66ba967a0f0143ffc
Josh Pollock <jpollock412@gmail.com>
2020-07-01 14:26:13 -0400

    init commit

---
+++ actions-test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Actions Test
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     actions-test
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Actions_Test
+ */
+
+// Your code starts here.
